4fd460cb354c69ad6723db51b64be28fe08aec12
rafalpawlisz <6074808+rafalpawlisz@users.noreply.github.com>
2020-10-21 19:28:48 +0200

    basic implementation

--- app/src/main/java/io/github/rafalpawlisz/boardgamesupport/MainActivity.kt
+++ app/src/main/java/io/github/rafalpawlisz/boardgamesupport/MainActivity.kt
@@ -1,11 +1,24 @@
 package io.github.rafalpawlisz.boardgamesupport
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    val random = Random()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_generate.setOnClickListener {
+            random.nextInt(10).toString().apply {
+                text_top_left.text = this
+                text_top_right.text = this
+                text_bottom_left.text = this
+                text_bottom_right.text = this
+            }
+        }
     }
 }
