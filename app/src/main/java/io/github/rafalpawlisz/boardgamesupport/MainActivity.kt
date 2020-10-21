026b2af61a0c2af6f6dac4abe20359eaf8dbc965
rafalpawlisz <6074808+rafalpawlisz@users.noreply.github.com>
2020-10-21 20:42:00 +0200

    implement action bar menu

--- app/src/main/java/io/github/rafalpawlisz/boardgamesupport/MainActivity.kt
+++ app/src/main/java/io/github/rafalpawlisz/boardgamesupport/MainActivity.kt
@@ -1,6 +1,8 @@
 package io.github.rafalpawlisz.boardgamesupport
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
@@ -19,6 +21,20 @@ class MainActivity : AppCompatActivity() {
                 text_bottom_left.text = this
                 text_bottom_right.text = this
             }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_action_bar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.cube -> true
+            R.id.imago -> true
+            R.id.wielki_zaklad -> true
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
